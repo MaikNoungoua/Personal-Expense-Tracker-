5633f9ec9d00f89f0a0ad8b84fc4c9b692437bc1
Maik Noungoua <noungouamaik@gmail.com>
2025-08-17 03:09:16 +0100

    B: Api server up and running, get methods ok, The next push will include at laeast path parameters

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import fastapi
+from fastapi import FastAPI 
+
+app = FastAPI() # Creates an API object that will initialise the API in our code. 
+
+# We are beginning to define the endpoints that our API will give access to.
+
+@app.get("/")
+def home():
+    return {"Data" : "test"}
+
+@app.get("/about/")
+def about():
+    return {"Data" : "This is the data on the endpoint about."}
+
